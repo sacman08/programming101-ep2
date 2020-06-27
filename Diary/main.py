b9ba849127511220b65b310156c8247cddfbf762
A. Sacman <sacmail08@gmail.com>
2020-06-27 09:20:45 -0400

    Changes to Diary/main.py to allow multi line entries. Changed file to log file extensions.

--- Diary/main.py
+++ Diary/main.py
@@ -1,22 +1,30 @@
-#Diary Application
-#TODO Create escape keystroke to allow multiple lines with enter key. Currently only allows typing until you hit enter 
+#Logging Application
+#TODO Add more descriptive date time for log file include year and time.
 
 #! /usr/bin/python
 from datetime import datetime
 
 def get_daily_input():
-    daily_diary = input("Write you daily thoughts:\n")
-    return daily_diary
+    daily_diary = []
+    print('Write your log. Press enter twice to escape.')
+    while True:
+        daily_d = input()
+        if daily_d:
+            daily_diary.append('\n')
+            daily_diary.append(daily_d)
+        else:
+            break
+    return daily_d.join(daily_diary)
 
 def write_daily(diary):
-    filename = f'Day_{datetime.now().strftime("%j")}.diary'
+    filename = f'Day_{datetime.now().strftime("%j")}.log'
     with open(filename, 'w') as f:
         f.write(diary)
 
 def main():
     diary = get_daily_input()
     write_daily(diary)
-    print("Diary logged for today.")
+    print("File logged for today.")
 
 if __name__ == "__main__":
     main()
